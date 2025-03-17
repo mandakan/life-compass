95420412bc8513e5a26b4652e0ab6fe6fe317327
Mathias Andersson <m@thias.se>
2025-03-17 16:10:36 +0100

    feat: add Swedish translations for "back" and update HomePage and SettingsPage for localization

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { getUserData } from '../utils/storageService';
-import { useTheme } from '../context/ThemeContext';
 import { useNavigate } from 'react-router-dom';
 import CustomButton from '../components/CustomButton';
 import LanguageSwitcher from 'components/LanguageSwitcher';
+import { useTranslation } from 'react-i18next';
 
 const HomePage: React.FC = () => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -14,10 +15,10 @@ const HomePage: React.FC = () => {
   }, []);
 
   return (
-    <div className="bg-bg text-text flex min-h-screen flex-col items-center justify-center p-8 font-sans">
+    <div className="bg-bg text-text flex min-h-screen flex-col items-center justify-center p-8 font-sans bg-gradient">
       <h1 className="mb-6 text-center text-2xl font-bold">Livskompass</h1>
       <CustomButton onClick={() => navigate('/create-life-compass')}>
-        Skapa Livskompass
+        {t('create_life_compass', 'Skapa Livskompass')}
       </CustomButton>
       <div className="mt-4">
         <LanguageSwitcher />
