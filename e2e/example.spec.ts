2b35e7106fd88a838cbe8b6d2b323b5eed1f167e
Mathias Andersson <m@thias.se>
2025-03-18 09:46:09 +0100

    feat: add end-to-end tests for Life Compass app and update workflow for actions/checkout

--- e2e/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('homepage has expected title', async ({ page }) => {
-  await page.goto('/');
-  // Wait until the document.title is set
-  await page.waitForFunction(() => document.title !== '');
-  await expect(page).toHaveTitle(/(Life Compass|Livskompass)/);
-});
