3807625de1f8b7e662a743782121d7182ad83cf3
Mathias Andersson <m@thias.se>
2025-03-20 16:53:36 +0100

    feat: add DEV environment variable to ImportMetaEnv interface

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 declare interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly BASE_URL: string;
+  readonly DEV: boolean;
   // Add other environment variables here as needed, for example:
   // readonly VITE_API_URL: string;
 }
