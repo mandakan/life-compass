38dc4f7f7a51611ed1208a2e38672ebe777167ce
Mathias Andersson <m@thias.se>
2025-03-24 10:29:16 +0100

    Remove Modal component and update utility function for improved type safety

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx } from 'clsx';
 
-export function cn(...args: any[]) {
-  return clsx(...args);
+export function cn(...inputs: Parameters<typeof clsx>): string {
+  return clsx(...inputs);
 }
